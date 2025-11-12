aba1b56b9436f4d6fc757a8ea219265b41131ec4
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2025-11-12 14:28:43 +0000

    Restructured files

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ViewStyle, } from 'react-native';
+
+export default function App() {
+  return (
+  );
+}
+
+const styles = StyleSheet.create<{
+  container: ViewStyle;
+  textarea: ViewStyle;
+  box: ViewStyle;
+}>({
+  textarea: {
+    margin: 100,
+    padding: 10,
+    width: '100%',
+    height: "100%",
+    backgroundColor: "green",
+  },
+  box: {
+
+  },
+});
---
+++ src/components/screens/kasiaScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StatusBar } from "react-native";
+
+function kasiaScreen() {
+    <View style={styles.container}>
+      <View style={styles.textarea} >
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>I Love BABY KASIA</Text>
+        <Text>I Love BABY KASIA</Text>
+        <Text>I Love BABY KASIA</Text>
+      </View>
+      <View style={styles.box} >
+
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+}
